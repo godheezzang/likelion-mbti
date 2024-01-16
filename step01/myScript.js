d29f950fe663a441611cd579d62305cbdd2946cd
gahee <930_10@naver.com>
2024-01-16 14:40:53 +0900

    step01

---
+++ step01/myScript.js
@@ -0,0 +1,10 @@
+var lotto=[];
+while (lotto.length < 6) {
+    var num = parseInt(Math.random() * 45 + 1);
+    if (lotto.indexOf(num) == -1) {
+        lotto.push(num);
+    }
+}
+
+lotto.sort((a,b)=>a-b);
+document.write(lotto);
